dd4e74042f364038145280d0df2111d6d885b969
Ashraf-2 <hasanashraful03@gmail.com>
2024-07-09 00:34:25 +0600

    added python date and math library

---
+++ Python_math/1_intro_math.py
@@ -0,0 +1,24 @@
+import math
+
+x = min(13,3,4)
+print(x)
+y = max(23,4,1)
+print(y)
+
+a = abs(-12.3)
+print(a)
+
+b = pow(2,3)        # pow(base, power)
+print(b)
+
+c = math.sqrt(64)   # squire root of 64 in float
+print(c)    # output: 8.0
+
+d = math.ceil(23.231)       # round a number upward toward a nearest integer
+print(d)        # 24
+
+e = math.floor(39.9)       #  round a number downward toward a nearest integer
+print(e)        # output: 39
+
+pi = math.pi        # pi number = 3.14159...
+print(pi)
