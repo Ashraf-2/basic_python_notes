0650c19fe8ff4a05c09ecd4c85c7472f5e5a075e
Ashraf-2 <hasanashraful03@gmail.com>
2024-07-31 23:05:14 +0600

    added request module

---
+++ Practice_Problem/4_non_repeated_chr.py
@@ -0,0 +1,4 @@
+text = "hello world! We are looking at you. We will built a peaceful earth.A earth of borderoless countries."
+# text.join()
+list_chr = list(text.replace('!','').replace('.',''))
+print(list_chr)
---
+++ Practice_Problem/5_count_freqeuncy.py
@@ -0,0 +1,7 @@
+import string 
+text = "a little effort can bring a lot change.a little effort can bring a lot change"
+text = text.split()
+dict= {}
+
+for i in text:
+    print()
